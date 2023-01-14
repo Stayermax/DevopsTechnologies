45ed1bee2a8b6e31157f1619e102d5a2ed749f6d
Stayermax <vitalkrat@gmail.com>
2023-01-15 00:34:58 +0200

    postgres connector updated and twitter database uploaded to postgres

--- connectors/postgres_connector.py
+++ connectors/postgres_connector.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 import logging
+import pandas as pd
 from utils.text_colors import bcolors
 
 class PostgresConnector:
@@ -20,7 +21,7 @@ class PostgresConnector:
         self.session = self.SessionMaker()
         self.base = declarative_base()
 
-    def create_session(self):
+    def start_session(self):
         self.session = self.SessionMaker()
         return self.session
 
@@ -40,8 +41,8 @@ class PostgresConnector:
     def run_query_without_output(self, query: str):
         self.engine.execute(query)
 
-    def run_set_of_queries_without_output(self, set_of_queries: list):
-        self.create_session()
+    def run_list_of_queries_without_output(self, set_of_queries: list[str]):
+        self.start_session()
         try:
             for query in set_of_queries:
                 self.session.query(text(query))
@@ -51,8 +52,8 @@ class PostgresConnector:
         finally:
             self.close_session()
 
-    def run_set_of_queries_with_output(self, set_of_queries: list):
-        self.create_session()
+    def run_list_of_queries_with_output(self, set_of_queries: list[str]):
+        self.start_session()
         res = None
         try:
             res = []
@@ -64,3 +65,40 @@ class PostgresConnector:
         finally:
             self.close_session()
             return res
+
+    def insert_df_to_table(self, df: pd.DataFrame, table_name: str):
+        df.to_sql(table_name, self.engine, index=False, if_exists='replace')
+        logging.info(f"Dataframe was inserted into database with name '{table_name}'")
+
+
+    def append_df_to_table(self, df: pd.DataFrame, table_name: str):
+        df.to_sql(table_name, self.engine, index=False, if_exists='append')
+        logging.info(f"Dataframe was appended to table '{table_name}'")
+    def insert_dict_into_table(self, dictionary: dict, table_name: str):
+        keys = dictionary.keys()
+        values = [f"'{str(dictionary[key])}'" for key in dictionary.keys()]
+        query = f"INSERT INTO {table_name} ({', '.join(keys)}) VALUES ({', '.join(values)});"
+        self.engine.execute(query)
+        logging.info(f"Dictionary was inserted into table '{table_name}'")
+
+    def insert_list_of_dicts_into_table(self, list_of_dicts: list[dict], table_name: str):
+        self.start_session()
+        try:
+            for dictionary in list_of_dicts:
+                keys = dictionary.keys()
+                values = [f"'{str(dictionary[key])}'" for key in dictionary.keys()]
+                query = f"INSERT INTO {table_name} ({', '.join(keys)}) VALUES ({', '.join(values)});"
+                logging.info(f"Executing query: {query}")
+                self.session.execute(query)
+            self.session.commit()
+            logging.info(f"List of dictionaries was inserted into table '{table_name}'")
+        except Exception as e:
+            logging.error(e)
+        finally:
+            self.close_session()
+
+    def delete_table(self, table_name: str):
+        query = f"DROP TABLE {table_name}"
+        self.engine.execute(query)
+        logging.info(f"Table '{table_name}' was dropped")
+
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from connectors.postgres_connector import PostgresConnector
+import os
 import yaml
+import logging
+import pandas as pd
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
 if __name__ == '__main__':
     with open('configs/db_configs.yml', 'r') as file:
         db_configuration = yaml.safe_load(file)['twitter_db']
     pg_connector = PostgresConnector(db_configuration)
+    for filename in os.listdir('data'):
+        db_name = filename.strip('.csv').lower()
+        df = pd.read_csv('data/Tweet.csv')
+        pg_connector.insert_df_to_table(df, db_name)
