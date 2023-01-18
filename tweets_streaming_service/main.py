280af0df3c00687b52d3895266a44653a078b4f7
Stayermax <vitalkrat@gmail.com>
2023-01-19 00:30:01 +0200

    tweepy is working without extended tweets

---
+++ tweets_streaming_service/main.py
@@ -0,0 +1,18 @@
+from connectors.postgres_connector import PostgresConnector
+# from tweets_streaming_service.src.twitter_client import TwitterClient
+from tweets_streaming_service.src.twitter_client_v2 import TwitterClient
+import os
+import yaml
+import logging
+import pandas as pd
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
+
+
+if __name__ == '__main__':
+
+    # Use Twitter API
+    twitter_client = TwitterClient('config/twitter_api_keys.yml')
+    twitter_client.search_tweets(query='israel')
+    # twitter_client.get_user(username='StayermaxProj')
+    # twitter_client.get_user_tweets()
--- tweets_streaming_service/src/twitter_client_v2.py
+++ tweets_streaming_service/src/twitter_client_v2.py
@@ -1,3 +1,5 @@
+import logging
+
 import tweepy
 import yaml
 import json
@@ -23,15 +25,63 @@ class TwitterClient():
             app_id = account_dict['app_id']
             pin = account_dict['pin']
 
-        auth = tweepy.OAuth1UserHandler(
+        self.auth = tweepy.OAuth1UserHandler(
             api_key, api_key_secret, access_token, access_token_secret
         )
 
-        client = tweepy.Client(bearer_token=bearer_token, return_type=dict)
-        a = client.search_recent_tweets(query='israel', tweet_fields=['attachments'])
+        self.client = tweepy.Client(bearer_token=bearer_token, return_type=dict)
+
+    def search_tweets(self, query: str):
+        all_fields = [
+            "attachments",
+            "author_id",
+            "context_annotations",
+            "conversation_id",
+            "created_at",
+            "entities",
+            "geo",
+            "id",
+            "in_reply_to_user_id",
+            "lang",
+            "possibly_sensitive",
+            "public_metrics",
+            "referenced_tweets",
+            "reply_settings",
+            "source",
+            "text",
+            "withheld"
+        ]
+        minimum_fields = [
+            'id',
+            'text'
+        ]
+        all_tweets = []
+        next_token = None
+
+        while(len(all_tweets)<20):
+            response = self.client.search_recent_tweets(query=query,
+                                                        tweet_fields=all_fields,
+                                                        next_token=next_token,
+                                                        max_results=10)
+            result_count = response['meta']['result_count']
+            if result_count > 0:
+                all_tweets += response['data']
+                logging.info(f" Tweets collected: {len(all_tweets)}")
+                if 'next_token' in response['meta'].keys():
+                    next_token = response['meta']['next_token']
+                else:
+                    logging.info(f"Next token not found")
+                    break
+        # for tweet in all_tweets:
+        #     print(tweet)
+        json_pretty_print(all_tweets)
+        logging.info(f"overall tweets collected: {len(all_tweets)}")
+
+
+    def get_user(self, username: str):
+        a = self.client.get_user(username=username)
         json_pretty_print(a)
 
-        # a = client.get_user(username="GretaThunberg")
-        # json_pretty_print(a)
-
-        # self.api.GetUser(screen_name='StayermaxProj')
+    def get_user_tweets(self, id: str = None):
+        tweets = self.client.get_users_tweets(id = "1496886348338405382")
+        json_pretty_print(tweets)
